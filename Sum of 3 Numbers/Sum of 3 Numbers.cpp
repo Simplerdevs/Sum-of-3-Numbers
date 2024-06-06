984a3f6a44a5058340368e3e7c24f876dcdb925b
Simplerdevs <tplg7839@gmail.com>
2024-06-06 09:18:53 -0700

    Add project files.

---
+++ Sum of 3 Numbers/Sum of 3 Numbers.cpp
@@ -0,0 +1,29 @@
+
+/* A program to ask the user to enter three numbers Then print the sum on screen */
+
+#include <iostream>
+using namespace std;
+
+int main()
+
+{
+
+    int num1, num2, num3;
+
+    cout << "Please Enter The First Number " << endl;
+    cin >> num1;
+
+    cout << "Please Enter The Second Number " << endl;
+    cin >> num2;
+
+    cout << "Please Enter The Third Number " << endl;
+    cin >> num3;
+
+
+    cout << "The sum of three Number Entered is : " << num1 + num2 + num3 << endl;
+
+
+    return 0;
+
+    // created by @ilyes_trabelsi
+}
